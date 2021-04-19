bf8549ec453efe6089077377fa0018eb1f4559ca
rubenfgr <rubenfgr87@outlook.com>
2021-04-19 10:51:02 +0200

    db configuration on .env and configuration & testing connection ok

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { InjectConnection, TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import configuration, { EnvironmentVariables } from './config/configuration';
@@ -11,13 +13,31 @@ import configuration, { EnvironmentVariables } from './config/configuration';
       load: [configuration],
       cache: true,
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService<EnvironmentVariables>) => ({
+        type: 'mysql',
+        host: configService.get('db.host'),
+        port: configService.get<number>('db.port'),
+        username: configService.get('db.user'),
+        password: configService.get('db.password'),
+        database: configService.get('db.database'),
+        entities: [],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {
-  constructor(private configService: ConfigService<EnvironmentVariables>) {
-    console.log(configService);
-    console.log(configService.get('port'));
+  constructor(
+    @InjectConnection('default')
+    private connectionDefault: Connection,
+  ) {
+    if (this.connectionDefault.isConnected) {
+      console.log(`Database "${connectionDefault.name}" Online!`);
+    }
   }
 }
--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -2,20 +2,24 @@ export default () => ({
   node_env: process.env.NODE_ENV,
   port: parseInt(process.env.PORT, 10) || 8880,
   db: {
+    type: process.env.DB_TYPE,
     host: process.env.DB_HOST,
     port: parseInt(process.env.DB_PORT, 10) || 8890,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
+    synchronize: parseInt(process.env.DB_SYNCHRONIZE, 10) || 0,
   },
 });
 
 export interface EnvironmentVariables {
   node_env: string;
   port: number;
+  'db.type': string;
   'db.host': string;
   'db.port': number;
   'db.user': string;
   'db.password': string;
   'db.database': string;
+  'db.synchronize': boolean;
 }
